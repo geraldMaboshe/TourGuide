cdc9364a6f669384b29d52cf67deb438e135414d
geraldMaboshe <maboshegerald1@gmail.com>
2019-08-24 17:25:12 +0200

    succcessfully connected to the google maps API

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import MapsTest from './MapsTest';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => MapsTest);
