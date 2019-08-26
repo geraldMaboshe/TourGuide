eeaa37b12c64800c20c343057da8a2ed86182557
geraldMaboshe <maboshegerald1@gmail.com>
2019-08-26 08:37:48 +0200

    asked for user permission

--- App.js
+++ App.js
@@ -1,49 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  PermissionsAndroid,
+  Platform
+} from "react-native";
+import MapScreen from "./MapScreen";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasMapPermission: false
+    };
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  componentDidMount() {
+    this.requestFineLocation();
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  async requestFineLocation() {
+    try {
+      if (Platform.OS === "android") {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          this.setState({ hasMapPermission: true });
+        }
+      } else {
+        this.setState({ hasMapPermission: true });
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    if (this.state.hasMapPermission) {
+      return <MapScreen />;
+    }
+    return null;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = StyleSheet.create({});
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import MapsTest from './MapsTest';
+//import MapsTest from './MapsTest';
 
 
-AppRegistry.registerComponent(appName, () => MapsTest);
+AppRegistry.registerComponent(appName, () => App);
